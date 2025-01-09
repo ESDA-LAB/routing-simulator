ec9c7599fac2d54574290c96e2531b52de86cfea
nck9665 <166904819+nck9665@users.noreply.github.com>
2025-01-09 13:33:21 +0200

    Update shortest_path_interface.py

--- DEMO/shortest_path_interface.py
+++ DEMO/shortest_path_interface.py
@@ -1,1 +1,9 @@
+from abc import ABC, abstractmethod
+from typing import List, Tuple
+
+class ShortestPathInterface(ABC):
+    @abstractmethod
+    def FindRoute(self, start: Tuple[int, int], end: Tuple[int, int], obstacles: List[Tuple[int, int]], 
+                  map_size: Tuple[int, int]) -> int:
+        pass
 
